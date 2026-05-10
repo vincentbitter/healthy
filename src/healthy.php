0bf70affbba35713212403252c3dc52e46c59830
Vincent Bitter <5149862+vincentbitter@users.noreply.github.com>
2026-05-10 15:41:58 +0000

    docs: Shorten plugin description.

--- src/healthy.php
+++ src/healthy.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: Healthy
  * Plugin URI: https://github.com/vincentbitter/healthy
- * Description: Provides a clean, reliable health endpoint architecture for WordPress environments, suitable for uptime monitoring, Kubernetes probes and automated infrastructure checks.
+ * Description: Provides a health endpoint for WordPress environments, suitable for uptime monitoring, Kubernetes probes and automated infrastructure checks.
  * Version: 0.0.0
  * Author: Vincent Bitter
  * Author URI: https://github.com/vincentbitter
