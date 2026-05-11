b56f20dd83188a96a06ad4763309eea6f95ed4c5
Vincent Bitter <5149862+vincentbitter@users.noreply.github.com>
2026-05-11 22:58:57 +0000

    chore: Restoring .gitconfig doesn't work.

--- .devcontainer/scripts/post-start.sh
+++ .devcontainer/scripts/post-start.sh
@@ -2,13 +2,6 @@
 set -e
 echo "=== Post-start ==="
 
-# Restore .gitconfig in Codespaces
-PERSIST_GIT="/workspaces/.codespaces/shared/git/.gitconfig"
-if [ -f "$PERSIST_GIT" ] && [ ! -L ~/.gitconfig ]; then
-    echo "Restoring .gitconfig from $PERSIST_GIT..."
-    ln -s "$PERSIST_GIT" ~/.gitconfig
-fi
-
 # Composer install
 echo "Running composer install..."
 cd src
