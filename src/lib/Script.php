3afef917db02ec34057511662abd3fe5bb15c238
Vincent Bitter <5149862+vincentbitter@users.noreply.github.com>
2026-06-03 01:36:10 +0200

    refactor: Comply with the WordPress coding standards.

--- src/lib/Script.php
+++ src/lib/Script.php
@@ -1,67 +1,145 @@
 <?php
+/**
+ * The Script class can be used to easily register scripts in WordPress.
+ *
+ * @package Healthy
+ */
 
 namespace Healthy;
 
-if (!defined('ABSPATH')) {
-    exit;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
 }
 
-class Script
-{
-    protected string $name;
-    protected string $file;
-    protected string $version;
-    protected array $deps = [];
-    protected array $localize = [];
-    protected ?string $textdomain = null;
-    protected bool $admin;
+/**
+ * The Script class can be used to easily register scripts in WordPress.
+ */
+class Script {
+	/**
+	 * Name of the script, used as identifier.
+	 *
+	 * @var string
+	 * */
+	protected string $name;
 
-    public function __construct(string $name, string $file, string $version = '1.0', bool $admin = false)
-    {
-        $this->name = $name;
-        $this->file = $file;
-        $this->version = $version;
-        $this->admin = $admin;
-    }
+	/**
+	 * File location of the file to include on the page.
+	 *
+	 * @var string
+	 */
+	protected string $file;
 
-    public function deps(array $deps): self
-    {
-        $this->deps = $deps;
-        return $this;
-    }
+	/**
+	 * Version of the script, to force cache invalidation.
+	 *
+	 * @var string
+	 */
+	protected string $version;
 
-    public function localize(string $object_name, array $data): self
-    {
-        $this->localize[] = [$object_name, $data];
-        return $this;
-    }
+	/**
+	 * Names of scripts the script depends on.
+	 *
+	 * @var array(string)
+	 */
+	protected array $deps = array();
 
-    public function translations(string $textdomain): self
-    {
-        $this->textdomain = $textdomain;
-        return $this;
-    }
+	/**
+	 * Variables that can be used in the script file.
+	 *
+	 * @var array(string, array<string, mixed>)
+	 */
+	protected array $localize = array();
 
-    public function register(): void
-    {
-        add_action($this->admin ? 'admin_enqueue_scripts' : 'wp_enqueue_scripts', function () {
-            wp_enqueue_script(
-                $this->name,
-                plugins_url($this->file, dirname(__DIR__) . '/index.php'),
-                $this->deps,
-                $this->version,
-                true
-            );
+	/**
+	 * Textdomain used for translations.
+	 *
+	 * @var string?
+	 */
+	protected ?string $textdomain = null;
 
-            // Localize
-            foreach ($this->localize as [$object_name, $data]) {
-                wp_localize_script($this->name, $object_name, $data);
-            }
+	/**
+	 * Include only in admin.
+	 *
+	 * @var bool
+	 */
+	protected bool $admin;
 
-            // Translations
-            if ($this->textdomain) {
-                wp_set_script_translations($this->name, $this->textdomain);
-            }
-        });
-    }
+	/**
+	 * Script constructor.
+	 *
+	 * @param string $name Name of the script, used as identifier.
+	 * @param string $file File location of the file to include on the page.
+	 * @param string $version Version of the script, to force cache invalidation.
+	 * @param bool   $admin Include only in admin.
+	 */
+	public function __construct( string $name, string $file, string $version = '1.0', bool $admin = false ) {
+		$this->name    = $name;
+		$this->file    = $file;
+		$this->version = $version;
+		$this->admin   = $admin;
+	}
+
+	/**
+	 * Set the dependencies for the script.
+	 *
+	 * @param array $deps Names of scripts the script depends on.
+	 * @return self
+	 */
+	public function deps( array $deps ): self {
+		$this->deps = $deps;
+		return $this;
+	}
+
+	/**
+	 * Localize the script with data.
+	 *
+	 * @param string $object_name Name of the JavaScript object to create.
+	 * @param array  $data        Data to pass to the JavaScript object.
+	 * @return self
+	 */
+	public function localize( string $object_name, array $data ): self {
+		$this->localize[] = array( $object_name, $data );
+		return $this;
+	}
+
+	/**
+	 * Set the textdomain for translations.
+	 *
+	 * @param string $textdomain Textdomain used for translations.
+	 * @return self
+	 */
+	public function translations( string $textdomain ): self {
+		$this->textdomain = $textdomain;
+		return $this;
+	}
+
+	/**
+	 * Register the script in WordPress.
+	 *
+	 * @return void
+	 */
+	public function register(): void {
+		add_action(
+			$this->admin ? 'admin_enqueue_scripts' : 'wp_enqueue_scripts',
+			function () {
+				wp_enqueue_script(
+					$this->name,
+					plugins_url( $this->file, dirname( __DIR__ ) . '/index.php' ),
+					$this->deps,
+					$this->version,
+					true
+				);
+
+				// Localize.
+				foreach ( $this->localize as [$object_name, $data] ) {
+					wp_localize_script( $this->name, $object_name, $data );
+				}
+
+				// Translations.
+				if ( $this->textdomain ) {
+					wp_set_script_translations( $this->name, $this->textdomain );
+				}
+			}
+		);
+	}
 }
